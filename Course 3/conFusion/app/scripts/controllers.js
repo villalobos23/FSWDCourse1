a2c82ddcb335c7ca990b07d059cc2f0aafd387c8
Luis J. Villalobos <villalobos23@gmail.com>
2017-05-07 22:56:23 -0400

    pending changes

--- Course 3/conFusion/app/scripts/controllers.js
+++ Course 3/conFusion/app/scripts/controllers.js
@@ -8,7 +8,19 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+            $scope.dishes = menuFactory.
+                    getDishes().
+                    then(
+                            function(response) {
+                                $scope.dishes = response.data;
+                                $scope.showMenu = true;
+                            },
+                            function(response) {
+                                $scope.message = "Error: "+response.status + " " + response.statusText;
+                            }
+                   );
 
                         
             $scope.select = function(setTab) {
@@ -70,9 +82,19 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
+            $scope.dish = {};
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+            menuFactory.getDish(parseInt($stateParams.id,10))
+                    .then(
+                        function(response){
+                            $scope.dish = response.data;
+                            $scope.showDish=true;
+                        },
+                        function(response) {
+                            $scope.message = "Error: "+response.status + " " + response.statusText;
+                        }
+                    );
             
         }])
 
@@ -98,7 +120,21 @@ angular.module('confusionApp')
             function($scope,corporateFactory,menuFactory){
                 $scope.featuredPromotion = menuFactory.getPromotion(0);
                 $scope.featuredChef = corporateFactory.getLeader(3);
-                $scope.featuredDish = menuFactory.getDish(0);
+                $scope.featuredDish = {};
+                
+                $scope.showDish = false;
+                $scope.message="Loading ...";
+
+                menuFactory.getDish(0)
+                        .then(
+                            function(response){
+                                $scope.featuredDish = response.data;
+                                $scope.showDish = true;
+                            },
+                            function(response) {
+                                $scope.message = "Error: "+response.status + " " + response.statusText;
+                            }
+                        );
             }])
         .controller("AboutController", ['$scope','corporateFactory',
             function($scope,corporateFactory){
